0e6e609d9b537ee5afc83e4be76e60d523f8dfa8
deathslayer5-dev <deathslayer532@gmail.com>
2026-02-02 16:15:03 -0500

    SHTR-03: Added Feeder

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -5,6 +5,7 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants.ShooterConstants;
@@ -17,25 +18,35 @@ public class ShooterSubsystem extends SubsystemBase {
 	//initialize required subsystems
 	private final HoodSubsystem hoodSubsystem = new HoodSubsystem();
 	private final FlywheelSubsystem flywheelSubsystem = new FlywheelSubsystem();
+	private final FeederSubsystem feederSubsystem = new FeederSubsystem();
 
 	private Supplier<AngularVelocity> flywheelVelocitySupplier = () -> DegreesPerSecond.of(1);
 
 	private PIDController hoodPIDController = new PIDController(
-			ShooterConstants.hoodKp, ShooterConstants.hoodKi, ShooterConstants.hoodKd
+			ShooterConstants.HoodKp, ShooterConstants.HoodKi, ShooterConstants.HoodKd
 	);
 	private PIDController flywheelPIDController = new PIDController(
-			ShooterConstants.flywheelKp, ShooterConstants.flywheelKi, ShooterConstants.flywheelKd
+			ShooterConstants.FlywheelKp, ShooterConstants.FlywheelKi, ShooterConstants.FlywheelKd
 	);
 
 
 	public ShooterSubsystem() {
 
 	}
+	public Command feed(){
+		return feederSubsystem.run();
+	}
+	public Command feed(Angle angle){
+		return feederSubsystem.setAngle(angle);
+	}
+	public Command stopFeeder(){
+		return feederSubsystem.reset();
+	}
 	public Command aimAt(Angle hoodAngle){
 		return hoodSubsystem.setAngle(hoodAngle);
 	}
 	public Command runShooter(){
-		if(flywheelPIDController == null){
+		if(flywheelVelocitySupplier == null){
 			DriverStation.reportWarning("Shooter velocity set to null, not running shooter", true);
 			return flywheelSubsystem.idle();
 		}
