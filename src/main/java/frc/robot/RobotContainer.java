be76c986484aa998e9428425f726848e300a3371
deathslayer5-dev <deathslayer532@gmail.com>
2026-02-03 14:44:30 -0500

    SHTR-14: Improved Feeder PID and included Beam Breaks

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,7 @@
 package frc.robot;
 
-import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.shooter.ShooterSubsystem;
-
-import static edu.wpi.first.units.Units.Degrees;
 
 public class RobotContainer {
     public RobotContainer() {
