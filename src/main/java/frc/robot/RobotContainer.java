0c724269712e4a2cb2876f59177f7f41fc00c109
deathslayer5-dev <deathslayer532@gmail.com>
2026-02-07 18:05:18 -0500

    SHTR-07: Added main Shooter Subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,12 @@
 package frc.robot;
 
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
 
 public class RobotContainer {
     public RobotContainer() {
