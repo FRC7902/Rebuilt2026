2b2a5a0c71c1e5e86613e785ef7c0e75763e737e
Jasper-World <jaspersiyu@outlook.com>
2026-02-08 17:58:57 -0500

    I did the subsystem currently working on the bindings and the command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,17 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class RobotContainer {
+  public final static IntakeSubsystem m_funnelIndexerSubsystem = new IntakeSubsystem();
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
