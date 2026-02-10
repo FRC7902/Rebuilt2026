8d44a0f2b1d27bcb4712631191dfefdc1dd401ef
deathslayer5-dev <deathslayer532@gmail.com>
2026-02-10 09:09:42 -0500

    SHTR-01: fixed flywheel

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,9 +14,9 @@ public final class Constants {
 
 		//Flywheel
 		public static final int FLYWHEEL_ID = 1;
-		public static final double FLYWHEEL_KP = -1;
-		public static final double FLYWHEEL_KI = -1;
-		public static final double FLYWHEEL_KD = -1;
+		public static final double FLYWHEEL_KP = 1;
+		public static final double FLYWHEEL_KI = 1;
+		public static final double FLYWHEEL_KD = 1;
 		public static final AngularVelocity FLYWHEEL_MAX_VELOCITY = RPM.of(5000);
 		public static final AngularAcceleration FLYWHEEL_MAX_ACCELERATION = RotationsPerSecondPerSecond.of(2500);
 		public static final MechanismGearing FLYWHEEL_GEARING = new MechanismGearing(GearBox.fromReductionStages(3, 4));
@@ -24,19 +24,19 @@ public final class Constants {
 		public static final Current FLYWHEEL_STATOR = Amps.of(50);
 		public static final Time FLYWHEEL_CLOSED_RATE = Seconds.of(1);
 		public static final Time FLYWHEEL_OPEN_RATE = Seconds.of(1);
-		public static final double FLYWHEEL_KS = -1;
-		public static final double FLYWHEEL_KV = -1;
-		public static final double FLYWHEEL_KA = -1;
+		public static final double FLYWHEEL_KS = 1;
+		public static final double FLYWHEEL_KV = 1;
+		public static final double FLYWHEEL_KA = 1;
 		public static final Distance FLYWHEEL_DIAMETER = Inches.of(1);
 		public static final Mass FLYWHEEL_MASS = Kilograms.of(1);
 		public static final AngularVelocity FLYWHEEL_LIMIT_LOW = RPM.of(-5000);
 		public static final AngularVelocity FLYWHEEL_LIMIT_HIGH = RPM.of(5000);
 
 		//Hood
-		public static final int HOOD_ID = 1;
-		public static final double HOOD_KP = -1;
-		public static final double HOOD_KI = -1;
-		public static final double HOOD_KD = -1;
+		public static final int HOOD_ID = 2;
+		public static final double HOOD_KP = 1;
+		public static final double HOOD_KI = 1;
+		public static final double HOOD_KD = 1;
 		public static final AngularVelocity HOOD_MAX_VELOCITY = RPM.of(5000);
 		public static final AngularAcceleration HOOD_MAX_ACCELERATION = RotationsPerSecondPerSecond.of(2500);
 		public static final MechanismGearing HOOD_GEARING = new MechanismGearing(GearBox.fromReductionStages(3, 4));
@@ -44,9 +44,9 @@ public final class Constants {
 		public static final Current HOOD_STATOR = Amps.of(50);
 		public static final Time HOOD_CLOSED_RATE = Seconds.of(1);
 		public static final Time HOOD_OPEN_RATE = Seconds.of(1);
-		public static final double HOOD_KS = -1;
-		public static final double HOOD_KV = -1;
-		public static final double HOOD_KA = -1;
+		public static final double HOOD_KS = 1;
+		public static final double HOOD_KV = 1;
+		public static final double HOOD_KA = 1;
 		public static final Distance HOOD_LENGTH = Inches.of(1);
 		public static final MomentOfInertia HOOD_MOI = KilogramSquareMeters.of(0.25);
 		public static final Angle HOOD_SOFT_LIMIT_LOW = Degrees.of(5);
@@ -56,10 +56,10 @@ public final class Constants {
 		public static final Angle HOOD_START_POSITION = Degrees.of(0);
 
 		//Feeder
-		public static final int FEEDER_ID = 2;
-		public static final double FEEDER_KP = -1;
-		public static final double FEEDER_KI = -1;
-		public static final double FEEDER_KD = -1;
+		public static final int FEEDER_ID = 3;
+		public static final double FEEDER_KP = 1;
+		public static final double FEEDER_KI = 1;
+		public static final double FEEDER_KD = 1;
 		public static final AngularVelocity FEEDER_MAX_VELOCITY = DegreesPerSecond.of(360);
 		public static final AngularAcceleration FEEDER_MAX_ACCELERATION = DegreesPerSecondPerSecond.of(180);
 		public static final MechanismGearing FEEDER_GEARING = new MechanismGearing(GearBox.fromReductionStages(3, 4));
@@ -67,9 +67,9 @@ public final class Constants {
 		public static final Current FEEDER_STATOR = Amps.of(40);
 		public static final Time FEEDER_CLOSED_RATE = Seconds.of(0.25);
 		public static final Time FEEDER_OPEN_RATE = Seconds.of(0.25);
-		public static final double FEEDER_KS = -1;
-		public static final double FEEDER_KV = -1;
-		public static final double FEEDER_KA = -1;
+		public static final double FEEDER_KS = 1;
+		public static final double FEEDER_KV = 1;
+		public static final double FEEDER_KA = 1;
 		public static final Distance FEEDER_LENGTH = Meters.of(0.2);
 		public static final Mass FEEDER_MASS = Kilogram.of(0.1);
 		public static final Angle FEEDER_HARD_LIMIT_LOW = Degrees.of(0);
@@ -77,8 +77,8 @@ public final class Constants {
 		public static final Angle FEEDER_START_POSITION = Degrees.of(0);
 		public static final double FEEDER_TIME_PERIOD = 2;
 
-		public static final int BEAM_BREAK_LEFT_ID = 3;
-		public static final int BEAM_BREAK_RIGHT_ID = 4;
-		public static final int BEAM_BREAK_TOP_ID = 5;
+		public static final int BEAM_BREAK_LEFT_ID = 4;
+		public static final int BEAM_BREAK_RIGHT_ID = 5;
+		public static final int BEAM_BREAK_TOP_ID = 6;
 	}
 }
