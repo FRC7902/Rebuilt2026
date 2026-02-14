dbe5b0739937c5e66e930b50c1c2dfcd72e1ca2b
Pix3ldog <Kate.Kag3@gmail.com>
2026-02-14 16:24:41 -0500

    I made the setspeed and getspeed methods
    I wrote the method for getspeed and setspeed and added the motor

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class Constants {
+    
+    public static class IndexerConstants {
+        public static final int INDEXER_MOTOR_CAN_ID = 20;
+     public static final int MOTOR_SPEED = 10;
+}
+}
