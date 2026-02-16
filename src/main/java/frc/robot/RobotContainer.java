4ff151db5ef5b1ff966a6c83fc678609c36f3d5c
deathslayer5-dev <deathslayer532@gmail.com>
2026-02-15 22:20:26 -0500

    SHTR-11: Renewed Constants for Interpolation Map

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,15 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.ShootCommand;
+import frc.robot.subsystems.shooter.FeederSubsystem;
+import frc.robot.subsystems.shooter.FlywheelSubsystem;
+import frc.robot.subsystems.shooter.HoodSubsystem;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
+
+import static edu.wpi.first.units.Units.*;
 
 public class RobotContainer {
     public RobotContainer() {
