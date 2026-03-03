6fd4e0818b2b6593b7385d362b5c92467377b5d1
aadhyasriram <152321380+aadhyasriram@users.noreply.github.com>
2026-03-02 23:29:44 -0500

    IDXR-99: Added oscillating indexer (works in sim)

--- src/main/java/frc/robot/Indexer.java
+++ src/main/java/frc/robot/Indexer.java
@@ -2,13 +2,11 @@ package frc.robot;
 
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IndexerConstants;
@@ -47,5 +45,13 @@ public class Indexer extends SubsystemBase{
     }
     public Command stopBalls() {
         return runOnce(() -> setSpeed(0));
+    }
+    public Command oscillatingIntake(){
+        return runOnce(() -> setSpeed(Math.sin(2*Math.PI * Timer.getFPGATimestamp()) * IndexerConstants.AGGRESIVE_MOTOR_SPEED));
+    }
+    @Override
+    public void periodic(){
+        SmartDashboard.putNumber("Speed of indexer motor", Math.sin(2*Math.PI * Timer.getFPGATimestamp()) * IndexerConstants.AGGRESIVE_MOTOR_SPEED);
+        SmartDashboard.putNumber("Voltage of indexer motor",m_indexerMotor.getMotorVoltage().getValueAsDouble());
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,17 +8,19 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.IndexerConstants;
+import frc.robot.commands.oscillatingIntake;
 
 public class RobotContainer {
   CommandXboxController m_driverController = new CommandXboxController(0);
-  Indexer m_indexer = new Indexer();
+  public static Indexer m_indexer = new Indexer();
   public RobotContainer() {
     configureBindings();
+    m_indexer.setDefaultCommand(new oscillatingIntake());
   }
 
   private void configureBindings() {
-    m_driverController.a().whileTrue(m_indexer.suckBalls(IndexerConstants.AGGRESIVE_MOTOR_SPEED));
-    m_driverController.b().whileTrue(m_indexer.spitBalls(IndexerConstants.AGGRESIVE_MOTOR_SPEED));
+    m_driverController.button(1).whileTrue(m_indexer.suckBalls(IndexerConstants.AGGRESIVE_MOTOR_SPEED));
+    m_driverController.button(2).whileTrue(m_indexer.spitBalls(IndexerConstants.AGGRESIVE_MOTOR_SPEED));
     (m_driverController.b().or(m_driverController.a()))
       .whileFalse(m_indexer.stopBalls());
   }
