7b0c8e01b545e04b5a1415a51d4f226cebc57343
Willie Pai <PaisWillie@gmail.com>
2026-03-25 23:45:48 -0400

    HTFX: Remove intake check from manual shoot button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -410,8 +410,7 @@ public class RobotContainer {
                 .onTrue(
                         Commands.sequence(
                                 Commands.waitSeconds(3),
-                                m_linearIntakeSubsystem.retract())
-                                .unless(m_driverController.L2()::getAsBoolean))
+                                m_linearIntakeSubsystem.retract()))
                 .onFalse(m_linearIntakeSubsystem.midpoint()
                         .unless(m_driverController.L2()::getAsBoolean));
 
