81e108356f94b4035e01a489483218c59ebd1c14
deathslayer5-dev <deathslayer532@gmail.com>
2026-05-24 19:30:36 -0400

    feat: Tracking pose

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "AdvantageScope";
+  public static final String MAVEN_NAME = "AdvantageKit";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 230;
-  public static final String GIT_SHA = "f4598ae5ab52e5875697d16b6297b435cc67d5c1";
-  public static final String GIT_DATE = "2026-05-24 17:08:10 EDT";
+  public static final int GIT_REVISION = 232;
+  public static final String GIT_SHA = "98bdf68c670409b514aa56d6b8bbc50667145e88";
+  public static final String GIT_DATE = "2026-05-24 19:30:36 GMT-04:00";
   public static final String GIT_BRANCH = "feature/AdvantageKit";
-  public static final String BUILD_DATE = "2026-05-24 17:33:14 EDT";
-  public static final long BUILD_UNIX_TIME = 1779658394542L;
+  public static final String BUILD_DATE = "2026-05-24 19:32:14 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1779665534946L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
